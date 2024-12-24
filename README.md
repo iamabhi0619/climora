07a216ff8496e64ddb94a752156ba7217fc6f47a
Abhishek Kumar <iamabhi0619@gmail.com>
2024-12-24 16:28:03 +0530

    Tesing Frontend

--- README.md
+++
@@ -1,2 +0,0 @@
-# WeatherWatcher
-A simple and elegant weather web app built using HTML, CSS, and vanilla JavaScript. Provides real-time weather updates and forecasts with a clean user interface.
--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const searchBtn = document.querySelector(".city");
+const input = document.querySelector(".input");
+const temp = document.querySelector(".temp");
+
 
 searchBtn.addEventListener("click", () => {
-  console.log("Helo city Name");
+  getTemp()
 });
 
-navigator.geolocation.getCurrentPosition((position) => {
-  console.log(position);
-});
+
+async function getTemp() {
+    const cityName = input.value;
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=34f5d9f52d17d59d5c6fc1e5dc89804b`)
+    const data = await response.json();
+    console.log(data);
+    temp.innerHTML = (data.main.temp);
+}
+
+
